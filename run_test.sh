22bdbf1ac7d64fbdbe2171975b377e61f46c6c00
apple-guobk <baokui2011@163.COM>
2021-05-28 19:28:14 +0800

    test

---
+++ run_test.sh
@@ -0,0 +1,3 @@
+python -u test.py sup >> log/test-sup.log 2>&1 &
+
+python -u test.py unsup >> log/test-unsup.log 2>&1 &
--- test.py
+++ test.py
@@ -1,40 +1,70 @@
+################################
+# supervised simcse - origin
 from simcse import SimCSE
 import json
 import numpy as np
-path_model = "/search/odin/guobk/data/simcse/simcse_roberta_zh_l12"
+import sys
+mode = sys.argv[1]
+maxRecall = 20
+minSim = 0.0
+if mode=='sup':
+    modeltag = 'simcse_sup'
+    path_model = "/search/odin/guobk/data/simcse/simcse_roberta_zh_l12_sup" 
+    path_target="/search/odin/guobk/data/bert_semantic/finetuneData_new_test/result-simcse_sup.json"
+else:
+    modeltag = 'simcse_sup'
+    path_model = "/search/odin/guobk/data/simcse/simcse_roberta_zh_l12"
+    path_target="/search/odin/guobk/data/bert_semantic/finetuneData_new_test/result-simcse_unsup.json"
 model = SimCSE(path_model)
 with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Docs.json','r') as f:
     Docs = json.load(f)
-with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Queries-142000.json','r') as f:
+with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Queries.json','r') as f:
     Queries = json.load(f)
-S = [Docs[i]['content'] for i in range(len(Docs))]
-V = model.encode(S)
-for i in range(len(Docs)):
-    # c = Docs[i]['content']
-    v = V[i]
-    v = v.numpy()
-    #v = np.float64(v)
-    d = v.dot(v)
-    v = list(v)
-    Docs[i]['sent2vec'] = v
-    if i%100==0:
-        print(i,len(Docs),d)
-V = [Docs[i]['sent2vec'] for i in range(len(Docs))]
-np.save('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Docs-simcse.npy',V)
-S = [Queries[i]['content'] for i in range(len(Queries))]
-V = model.encode(S)
-for i in range(len(Queries)):
-    # c = Docs[i]['content']
-    v = V[i]
-    v = v.numpy()
-    v = np.float64(v)
-    d = v.dot(v)
-    v = list(v)
-    Queries[i]['sent2vec'] = v
-    if i%100==0:
-        print(i,len(Queries),d)
+Sd = [Docs[i]['content'] for i in range(len(Docs))]
+Vd = model.encode(S)
+Sq = [Queries[i]['content'] for i in range(1000)]
+Vq = model.encode(Sq)
+Vd = Vd.numpy()
+Vq = Vq.numpy()
+R = []
+for i in range(len(Vq)):
+    s = Vd.dot(Vq[i])
+    idx = np.argsort(-s)
+    rec = [Sd[j]+'\t%0.4f'%s[j] for j in idx[:maxRecall] if s[j]>=minSim]
+    d = Queries[i]
+    d['rec_'+modeltag] = rec
+    R.append(d)
+with open(path_target,'w') as f:
+    json.dump(R,f,ensure_ascii=False,indent=4)
 
-# with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Queries-simcse.json','w') as f:
-#     json.dump(Queries,f,ensure_ascii=False,indent=4)
-V = [Queries[i]['sent2vec'] for i in range(len(Queries))]
-np.save('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Queries-simcse.npy',V)
+################################
+# unsupervised simcse - origin
+# from simcse import SimCSE
+# import json
+# import numpy as np
+# maxRecall = 20
+# minSim = 0.0
+# modeltag = 'simcse_sup'
+# path_model = "/search/odin/guobk/data/simcse/simcse_roberta_zh_l12"
+# path_target="/search/odin/guobk/data/bert_semantic/finetuneData_new_test/result-simcse_unsup.json"
+# model = SimCSE(path_model)
+# with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Docs.json','r') as f:
+#     Docs = json.load(f)
+# with open('/search/odin/guobk/data/bert_semantic/finetuneData_new_test/Queries.json','r') as f:
+#     Queries = json.load(f)
+# Sd = [Docs[i]['content'] for i in range(len(Docs))]
+# Vd = model.encode(Sd)
+# Sq = [Queries[i]['content'] for i in range(1000)]
+# Vq = model.encode(Sq)
+# Vd = Vd.numpy()
+# Vq = Vq.numpy()
+# R = []
+# for i in range(len(Vq)):
+#     s = Vd.dot(Vq[i])
+#     idx = np.argsort(-s)
+#     rec = [Sd[j]+'\t%0.4f'%s[j] for j in idx[:maxRecall] if s[j]>=minSim]
+#     d = Queries[i]
+#     d['rec_'+modeltag] = rec
+#     R.append(d)
+# with open(path_target,'w') as f:
+#     json.dump(R,f,ensure_ascii=False,indent=4)
